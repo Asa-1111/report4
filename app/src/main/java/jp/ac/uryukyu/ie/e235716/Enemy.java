7c17e11737b3d251a132a0ebcc6b3599ebe61dbb
Asahi Nkasone <e235716@ie.u-ryukyu.ac.jp>
2023-11-28 05:36:41 +0900

    fifth commit

--- app/src/main/java/jp/ac/uryukyu/ie/e235716/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235716/Enemy.java
@@ -8,11 +8,7 @@ package jp.ac.uryukyu.ie.e235716;
  *  boolean dead; //敵の生死状態。true=死亡。
  * Created by tnal on 2016/11/13.
  */
-public class Enemy {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+public class Enemy extends LivingThing{
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -21,29 +17,11 @@ public class Enemy {
      * @param attack モンスターの攻撃力
      */
     public Enemy (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
+        super(name,maximumHP,attack);
         System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
     }
 
-    /**
-     * Heroへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param hero 攻撃対象
-     */
-    public void attack(Hero hero){
-        if( hitPoint > 0 ){
-            int damage = (int)(Math.random() * attack);
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-            hero.wounded(damage);
-        }else{
-            int noDamage = 0;
-            System.out.println("モンスターは力尽きた");
-            hero.wounded(noDamage);
-        }
-    }
+    
 
     /**
      * 自身へ攻撃されたときのダメージ処理をするメソッド。
@@ -51,29 +29,13 @@ public class Enemy {
      * @param damage 受けたダメージ
      */
     public void wounded(int damage){
-        hitPoint -= damage;
-        if( hitPoint < 0 ) {
-            dead = true;
-            System.out.printf("モンスター%sは倒れた。\n", name);
+        setHitPoint(getHitPoint() - damage);
+        if( getHitPoint() < 0 ) {
+            setDead(true);
+            System.out.printf("モンスター%sは倒れた。\n", getName());
         }
     }
 
-    // getterとsetterメソッド
-     public String getName() {
-        return name;
-    }
-
-    public int getHitPoint() {
-        return hitPoint;
-    }
-
-    public int getAttack() {
-        return attack;
-    }
-
-    public boolean isDead() {
-        return dead;
-    }
-
+    
 
 }
